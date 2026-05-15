419a4f05985b0864350fdff72b5756343fccad6d
Matias Laime <matulai1221@gmail.com>
2026-05-15 00:50:01 -0300

    feat(improve-menu-ux): add toast base config

--- mesaAgil/app/_layout.tsx
+++ mesaAgil/app/_layout.tsx
@@ -8,6 +8,7 @@ import {
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'react-native';
+import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -21,12 +22,50 @@ export default function RootLayout() {
     return null;
   }
 
+  const toastConfig = {
+    success: (props: any) => (
+      <BaseToast
+        {...props}
+        style={{
+          borderLeftColor: '#58c400',
+          borderLeftWidth: 8,
+          height: 48
+        }}
+        contentContainerStyle={{
+          paddingHorizontal: 16
+        }}
+        text1Style={{
+          fontSize: 16,
+          fontFamily: 'Inter_600SemiBold'
+        }}
+      />
+    ),
+
+    error: (props: any) => (
+      <ErrorToast
+        {...props}
+        style={{
+          borderLeftColor: '#c40000',
+          borderLeftWidth: 8
+        }}
+        contentContainerStyle={{
+          paddingHorizontal: 16
+        }}
+        text1Style={{
+          fontSize: 16,
+          fontFamily: 'Inter_600SemiBold'
+        }}
+      />
+    )
+  };
+
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar />
+      <Toast config={toastConfig} />
     </ThemeProvider>
   );
 }
